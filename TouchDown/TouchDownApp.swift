ea5847ac10612c32ae53f8d011127672fd89fdcb
Oleg Kudimov <ovkudimov@gmail.com>
2021-11-21 23:16:04 +0300

    Initial Commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Oleg Kudimov on 11/21/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
